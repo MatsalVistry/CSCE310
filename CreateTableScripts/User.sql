a83eff681de4f0113bc87e97b4c5c3ffe56b48dc
MatsalVistry <vatsalmistry4796@gmail.com>
2022-11-10 18:29:58 -0600

    created users table

---
+++ CreateTableScripts/User.sql
@@ -0,0 +1,10 @@
+CREATE TABLE USERS
+(
+    User_ID INT NOT NULL AUTO_INCREMENT,
+    User_First_Name VARCHAR(50) NOT NULL,
+    User_Last_Name VARCHAR(50) NOT NULL,
+    User_Role VARCHAR(1) NOT NULL,
+    User_Email VARCHAR(255) NOT NULL,
+    User_Password VARCHAR(255) NOT NULL,
+    PRIMARY KEY(User_ID)
+);
--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var jQueryScript = document.createElement('script');
 jQueryScript.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 document.head.appendChild(jQueryScript);
 
-function doSomething() {
+function doSomething() 
+{
     $.ajax({
         type: "GET",
         url: "doSomething.php",
@@ -11,6 +12,25 @@ function doSomething() {
         },
         success: function(response) {
             console.log(response);
+        }
+    });
+}
+
+
+function updateIngredientQuantity()
+{
+    $.ajax({
+        type: "POST",
+        url: "ManagerBackend/Ingredients.php",
+        data: 
+        {
+            functionName: "updateIngredientQuantity",
+            name: name,
+            quantity: quantity
+        },
+        success: function(response) 
+        {
+            populateIngredientsView();
         }
     });
 }
