d2c8667ed19d9d22d8794450496ea68342afd0a4
MatsalVistry <vatsalmistry4796@gmail.com>
2022-11-10 18:51:25 -0600

    create table script for classes

---
+++ CreateTableScripts/Classes.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Classes
+(
+    Class_ID INT NOT NULL AUTO_INCREMENT,
+    Tutor_ID INT NOT NULL,
+    Class_MaxCapacity INT NOT NULL,
+    Class_CurrentCapacity INT NOT NULL,
+    Class_Name VARCHAR(255) NOT NULL,
+    Class_Date DATE NOT NULL,
+    Class_Duration FLOAT NOT NULL,
+    Class_Status VARCHAR(1) NOT NULL,
+
+    PRIMARY KEY(Class_ID),
+    FOREIGN KEY(Tutor_ID) REFERENCES Users(User_ID)
+);
