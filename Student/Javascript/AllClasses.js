c3566dbb445934942112fd5aa406322bac622828
justinhan07 <justinhan07@gmail.com>
2022-12-05 18:11:48 -0600

    Add button for tutor profile

--- Student/Javascript/AllClasses.js
+++ Student/Javascript/AllClasses.js
@@ -75,7 +75,7 @@ function populateClasses() {
                         var classDiv = document.createElement("div");
                         classDiv.classList.add("box");
                         classDiv.innerHTML += "<div class='cid' value=" + classes[i].id + "> ID: " + classes[i].id + "</div><br>";
-                        classDiv.innerHTML += "<div class='ctutorid' onclick=goToTutor("+classes[i].tutor_id+") value=" + classes[i].tutor_id + "> Tutor ID: " + classes[i].tutor_id + "</div><br>";
+                        classDiv.innerHTML += "<div class='ctutorid' value=" + classes[i].tutor_id + "> Tutor ID: " + classes[i].tutor_id + "</div><br>";
                         classDiv.innerHTML += "<div class='ctutorname' value=" + classes[i].tutor_name + "> Tutor Name: " + classes[i].tutor_name + "</div><br>";
                         classDiv.innerHTML += "<div class='cmaxcapacity' value=" + classes[i].max_capacity + "> Max Capacity: " + classes[i].max_capacity + "</div><br>";
                         classDiv.innerHTML += "<div class='ccurrentcapacity' value=" + classes[i].current_capacity + "> Current Capacity: " + classes[i].current_capacity + "</div><br>";
@@ -83,7 +83,7 @@ function populateClasses() {
                         classDiv.innerHTML += "<div class='cdate' value=" + classes[i].date + "> Date: " + classes[i].date + "</div><br>";
                         classDiv.innerHTML += "<div class='cduration' value=" + classes[i].duration + "> Duration: " + classes[i].duration + "</div><br>";
                         classDiv.innerHTML += "<div class='cstatus' value=" + classes[i].status + "> Status: " + classes[i].status + "</div><br>";
-
+                        
 
                         if (!currentClasses.includes(classes[i].id) && parseInt(classes[i].current_capacity) < parseInt(classes[i].max_capacity)) {
                             classDiv.innerHTML += '<button class="enroll" onclick="enroll(this)" value=' + classes[i].id + '>Enroll</button>';
@@ -92,7 +92,8 @@ function populateClasses() {
                         if (currentClasses.includes(classes[i].id)) {
                             classDiv.innerHTML += '<button class="unenroll" onclick="unenroll(this)" value=' + classes[i].id + '>Unenroll</button>';
                         }
-
+                        
+                        classDiv.innerHTML += "<button class='unenroll' onclick=goToTutor("+classes[i].tutor_id+")>Tutor Profile</button>";
                         classDiv.innerHTML += "<br><br>";
 
                         display.appendChild(classDiv);
