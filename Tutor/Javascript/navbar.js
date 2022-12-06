bf8f5ca3bfd92f6985f3466d47ae4e773d2b519a
MatsalVistry <vatsalmistry4796@gmail.com>
2022-12-05 19:01:39 -0600

    fixed student navbar on teacher profile

--- Tutor/Javascript/navbar.js
+++ Tutor/Javascript/navbar.js
@@ -14,3 +14,22 @@ function logout() {
     localStorage.clear();
     window.location.href = "../Login/login.html";
 }
+
+function AllClasses() {
+    window.location = "../Student/AllClasses.html";
+}
+
+function MyClasses()
+{
+    window.location = "../Student/MyClasses.html";
+}
+
+function PastClasses()
+{
+    window.location = "../Student/PastClasses.html";
+}
+
+function Profile()
+{
+    window.location = "../Student/Profile.html";
+}
